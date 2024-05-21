7fbf8760944cd7002c12dba05ee3544061593a46
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-05-21 18:23:57 -0500

    Initial commit

---
+++ src/test/java/com/ricardo/gutendex/gutendex/GutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.gutendex.gutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
